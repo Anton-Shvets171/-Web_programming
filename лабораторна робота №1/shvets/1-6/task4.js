93a2c2c07e6c476bf3dab9f48b372b73de0b5ded
Anton-Shvets171 <81146028+Anton-Shvets171@users.noreply.github.com>
2021-10-27 10:15:58 +0300

    *

---
+++ лабораторна робота №1/shvets/1-6/task4.js
@@ -0,0 +1,20 @@
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    while (0 !== currentIndex) {
+  
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
+  
+  // Used like so
+  var arr = [2, 11, 37, 42];
+  shuffle(arr);
+  console.log(arr);
---
+++ лабораторна робота №1/shvets/7/task7/Ship.js
@@ -0,0 +1,10 @@
+
+class Ship {
+  constructor (options) {
+    this.id = options.id
+    this.model = options.model
+    //this.on_pier = options.on_pier
+  }
+}
+
+module.exports = Ship
