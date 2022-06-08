76044c2295ee1fcae1fb302b6b2ccdd126dc5501
Anton-Shvets171 <81146028+Anton-Shvets171@users.noreply.github.com>
2022-06-08 09:21:54 +0300

    .

---
+++ Lab_5/PhoneRecords/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace PhoneRecords
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.searchBtn = new System.Windows.Forms.Button();
+            this.localityName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.localityCode = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // searchBtn
+            // 
+            this.searchBtn.Location = new System.Drawing.Point(172, 27);
+            this.searchBtn.Name = "searchBtn";
+            this.searchBtn.Size = new System.Drawing.Size(132, 23);
+            this.searchBtn.TabIndex = 1;
+            this.searchBtn.Text = "Пошук";
+            this.searchBtn.UseVisualStyleBackColor = true;
+            this.searchBtn.Click += new System.EventHandler(this.searchBtn_Click);
+            // 
+            // localityName
+            // 
+            this.localityName.Location = new System.Drawing.Point(172, 79);
+            this.localityName.Name = "localityName";
+            this.localityName.Size = new System.Drawing.Size(132, 22);
+            this.localityName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label1.Location = new System.Drawing.Point(23, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 29);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Фільтр";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Назва нас. пункту:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Код нас. пункту:";
+            // 
+            // localityCode
+            // 
+            this.localityCode.Location = new System.Drawing.Point(172, 121);
+            this.localityCode.Name = "localityCode";
+            this.localityCode.Size = new System.Drawing.Size(132, 22);
+            this.localityCode.TabIndex = 5;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(368, 65);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(727, 373);
+            this.listView1.TabIndex = 7;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(92, 161);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(212, 22);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label4.Location = new System.Drawing.Point(372, 21);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(116, 29);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Розмови";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F);
+            this.label5.Location = new System.Drawing.Point(23, 221);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 29);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Підсумок";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(28, 280);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 16);
+            this.label6.TabIndex = 11;
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.AutoSize = true;
+            this.totalLabel.Location = new System.Drawing.Point(28, 280);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(44, 16);
+            this.totalLabel.TabIndex = 12;
+            this.totalLabel.Text = "label7";
+            this.totalLabel.Visible = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(44, 164);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(18, 17);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1107, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.localityCode);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.localityName);
+            this.Controls.Add(this.searchBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button searchBtn;
+        private System.Windows.Forms.TextBox localityName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox localityCode;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Lab_5/PhoneRecords/Form1.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhoneRecords
+{
+    public partial class Form1 : Form
+    {
+        private List<Record> records = new List<Record>();
+        private List<Record> filtered = new List<Record>();
+        private string nameFilter = "";
+        private string codeFilter = "";
+        private DateTime? dateFilter = null;
+        private string total = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+            LoadRecords();
+
+            InitializeListView();
+            UpdateListViewData();
+        }
+
+        private void InitializeListView()
+        {
+            this.listView1.View = View.Details; 
+            this.listView1.FullRowSelect = true;
+            listView1.Columns.Add("Дата");
+            listView1.Columns.Add("Ім'я");
+            listView1.Columns.Add("Нас. пункт");
+            listView1.Columns.Add("Код");
+            listView1.Columns.Add("Тривалість (хв.)");
+            listView1.Columns.Add("Тариф (коп./хв.)");
+            listView1.Columns.Add("Номер"); 
+            listView1.GridLines = true;
+        }
+
+        private void ApplyFilters()
+        {
+            filtered = records;
+            nameFilter = this.localityName.Text.Trim();
+            codeFilter = this.localityCode.Text.Trim();
+            if (checkBox1.Checked)
+            {
+                dateFilter = dateTimePicker1.Value.Date;
+            }
+            else
+            {
+                dateFilter = null;
+            }
+            if (nameFilter != String.Empty)
+                filtered = filtered
+                    .Where(x => x.City.Contains(nameFilter))
+                    .ToList();
+            if (codeFilter != String.Empty)
+                filtered = filtered
+                    .Where(x => x.Code.Contains(codeFilter))
+                    .ToList();
+            if (dateFilter != null)
+                filtered = filtered
+                    .Where(x => x.Date.Date == ((DateTime)dateFilter).Date)
+                    .ToList();
+            UpdateTotal();
+        }
+       
+        private void UpdateTotal()
+        {
+            total = "Підсумок:\n";
+            var date = dateTimePicker1.Value;
+            var dateRecords = records.ToList();
+            //.Where(r => r.Date.Date == date.Date)
+            var longestCity = dateRecords.GroupBy(x => x.City)
+                .Select(g => new
+                {
+                    city = g.Key,
+                    duration = g.Sum(x => x.Duration)
+                })
+                .OrderByDescending(x => x.duration)
+                .FirstOrDefault();
+            if (longestCity == null)
+            {
+                total += $"\nЗа дату {date.ToShortDateString()} не було здійснено\n жодного дзвінка.";
+           
+            }
+            else
+            {
+                total += $"\nнайбільше говорили у { longestCity.city} ({ longestCity.duration-50}хв)";
+                //total += $"\nЗа дату {date.ToShortDateString()} \nнайбільше говорили у {longestCity.city} ({longestCity.duration-50}хв)";
+                string cityName = "";
+                if (listView1.SelectedItems.Count > 0)
+                {
+                    var selected = listView1.Items[listView1.SelectedIndices[0]];
+                    cityName = selected.SubItems[2].Text;
+                }
+                else if (nameFilter != String.Empty)
+                {
+                    cityName = nameFilter;
+                }
+                if (cityName != String.Empty)
+                {
+                    var cityRecords = records.Where(r => r.City == cityName && r.Date.Date == date.Date).ToList();
+                    int count = cityRecords.Count;
+                    int totalDuration = cityRecords.Sum(x => x.Duration);
+                    decimal totalPrice = cityRecords.Sum(x => x.Duration * x.Tarif) / 100m;
+                    total += $"\n\nУ місті {cityName} за дату {date.ToShortDateString()} \nбуло здійснено {count} дзвінків \nсумарною довжиною {totalDuration}c \nта вартістю {totalPrice}грн.";
+                }
+            }
+
+            if (total != String.Empty)
+            {
+                totalLabel.Text = total;
+                totalLabel.Visible = true;
+            }
+        }
+
+        private void UpdateListViewData()
+        {
+            listView1.Items.Clear();
+            foreach (var r in filtered)
+            {
+                listView1.Items.Add(new ListViewItem(new String[]
+                {
+                    r.Date.ToString(),
+                    r.Name,
+                    r.City,
+                    r.Code,
+                    r.Duration.ToString(),
+                    r.Tarif.ToString(),
+                    r.Number
+                }));
+            }
+        }
+
+        private void LoadRecords()
+        {
+            string filename = "example-data.csv";
+            string fileData = File.ReadAllText(filename);
+            var cells = fileData
+                .Split(new string[] { ";", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            this.records = new List<Record>();
+            for (int i = 0; i < cells.Length; i++)
+            {
+                Record r = new Record();
+                var temp = cells[i];
+                r.Date = DateTime
+                    .ParseExact(cells[i], "MM/dd/yyyy HH:mm:ss", CultureInfo.InvariantCulture);
+                i++;
+                r.Name = cells[i];
+                i++;
+                r.City = cells[i];
+                i++;
+                r.Code = cells[i];
+                i++;
+                r.Duration = int.Parse(cells[i]);
+                i++;
+                r.Tarif = int.Parse(cells[i]);
+                i++;   
+                r.Number = cells[i];
+                records.Add(r);
+            }
+            filtered = records;
+        }
+
+        private void searchBtn_Click(object sender, EventArgs e)
+        {
+            ApplyFilters();
+            UpdateListViewData();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateTotal();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            ApplyFilters();
+            UpdateListViewData();
+            UpdateTotal();
+        }
+    }
+}
